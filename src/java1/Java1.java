b2880594999c80986f29b742c2465499573af6cc
Salma Walli <salma.abdelmonem.walli@gmail.com>
2022-08-02 20:13:19 +0200

    First commit

---
+++ src/java1/Java1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java1;
+
+
+
+/**
+ *
+ * @author M&G
+ */
+
+public class Java1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Table t = new Table();
+     t.setVisible(true);
+     
+    }
+    
+}
+
